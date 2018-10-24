64d1cf0a4ae66d664c64521c36b4594dd31d83b9
suotani <s.uotani.zetakansu@gmail.com>
2018-10-24 22:19:00 +0900

    pc版完成

--- site-copy/wordpress/unwide/index.js
+++ site-copy/wordpress/unwide/index.js
@@ -6,3 +6,17 @@ $(".header-list-parent").hover(
     $(this).find(".header-sub-list").hide();
   }
 );
+
+$(".search-icon").click(function(){
+  if($(".search-wrap").is(":hidden")){
+    $(".search-wrap").slideDown();
+    $("body").addClass("scroll-stop");
+    $(".header-icons .fa-search").addClass("hidden");
+    $(".header-icons .fa-times").removeClass("hidden");
+  }else{
+    $(".search-wrap").slideUp();
+    $("body").removeClass("scroll-stop");
+    $(".header-icons .fa-search").removeClass("hidden");
+    $(".header-icons .fa-times").addClass("hidden");
+  }
+});
